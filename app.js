7e5b21f1c8dd1301a25cea6143ac55edbd1d7146
Dejan Saric <dsaric@mail.usf.edu>
2017-07-25 14:24:08 -0400

    final

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const mustacheExpress = require('mustache-express')
+
+const app = express()
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+//Listening on root
+app.get('/todo/', function(req, res) {
+  // TODO write your code here
+  const data = {
+    todos: [
+      { task: 'Learn node basics' },
+      { task: 'Learn Express basics' },
+      { task: 'Learn Mustace' },
+      { task: 'Learn HTML forms with Express' },
+      { task: 'Learn about authentication' },
+      { task: 'Learn how to connect to PostgreSQL' },
+      { task: 'Learn how to create databases' },
+      { task: 'Learn SQL' },
+      { task: 'Learn how to connect to PostgreSQL from Node' },
+      { task: 'Learn how to use Sequelize' }
+    ]
+  }
+  res.render('todo', data)
+})
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!')
+})
